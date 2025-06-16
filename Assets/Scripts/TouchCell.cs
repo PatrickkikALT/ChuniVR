0daa88c7e4493e4b1cbe328833d1c2de2110d96f
otherpatrick <45902985+PatrickkikALT@users.noreply.github.com>
2025-06-16 15:10:53 +0200

    Cleaned up folders + testing hand tracking
    Hand tracking only works via Unity Editor because meta is greedy and doesnt implement it for builds

---
+++ Assets/Scripts/TouchCell.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
+
+
+public class TouchCell : MonoBehaviour {
+  public int btn;
+  private bool xrDeviceEnabled;
+  private void Start() {
+    if (XRDeviceSimulator.instance.isActiveAndEnabled) {
+      xrDeviceEnabled = true;
+    }
+  }
+
+  private void OnCollisionEnter(Collision other) {
+    if (other.gameObject.layer != 6) return;
+    if (other.transform.parent.parent.TryGetComponent(out HapticImpulsePlayer haptic)) 
+      SendHaptic(haptic);
+    SendBtn();
+  }
+
+  private void OnCollisionStay(Collision other) {
+    if (other.gameObject.layer != 6) return; 
+    if (other.transform.parent.parent.TryGetComponent(out HapticImpulsePlayer haptic)) 
+      SendHaptic(haptic);
+  }
+
+  private void OnCollisionExit(Collision other) {
+    if (other.gameObject.layer != 6) return;
+    ReleaseButton();
+  }
+
+  private bool SendHaptic(HapticImpulsePlayer haptic) {
+    if (xrDeviceEnabled) {
+      return false;
+    }
+    return haptic.SendHapticImpulse(0.6f, 0.1f);
+  }
+  
+  [ContextMenu("Test")]
+  public void SendBtn() {
+    ChuniIO.Instance.SendButtonToIO(btn);
+  }
+
+  public void ReleaseButton() {
+    ChuniIO.Instance.ReleaseButtonFromIO(btn);
+  }
+}
